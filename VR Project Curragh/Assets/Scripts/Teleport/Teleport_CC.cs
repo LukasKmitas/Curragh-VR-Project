9e8fb16dd16123cb8928169c84eb1d9dfa610177
Lukas Kmitas <lukaskmitas@hotmail.co.uk>
2023-08-09 16:36:44 +0100

    VR Movement/map complete

---
+++ VR Project Curragh/Assets/Scripts/Teleport/Teleport_CC.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teleport_CC : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTransform;
+
+    public Vector3 teleportCoordinates = new Vector3(0f, 0f, 0f);
+
+    private void Start()
+    {
+        Button teleportButton = GetComponent<Button>();
+        teleportButton.onClick.AddListener(TeleportPlayer);
+    }
+
+    public void TeleportPlayer()
+    {
+        if (playerTransform != null)
+        {
+            playerTransform.position = teleportCoordinates;
+        }
+        else
+        {
+            Debug.LogWarning("Error !!!");
+        }
+    }
+}
---
+++ VR Project Curragh/Assets/Scripts/Teleport/Teleport_RH.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teleport_RH : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 teleportCoordinates = new Vector3(0f, 0f, 0f);
+
+    private void Start()
+    {
+        Button teleportButton = GetComponent<Button>();
+        teleportButton.onClick.AddListener(TeleportPlayer);
+    }
+
+    public void TeleportPlayer()
+    {
+        playerTransform.position = teleportCoordinates;
+    }
+}
